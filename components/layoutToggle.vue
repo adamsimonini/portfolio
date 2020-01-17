397e2db209ac9a7e3524ed335a90bc7ce475d56e
Adam Simonini <adamsimonini@users.noreply.github.com>
2020-01-17 13:53:48 -0500

    leaving layout toggle as is for now

--- components/layoutToggle.vue
+++ components/layoutToggle.vue
@@ -1,7 +1,8 @@
 <template>
     <v-list-item @click="flipLayout()">
       <v-list-item-action>
-        <v-icon>{{`mdi-page-layout-sidebar-${this.layout + this.layout == 'left' ? 'right' : 'left'}`}}</v-icon>
+        <v-icon>{{`mdi-page-layout-sidebar-${this.layout}`}}</v-icon>
+        <!-- <v-icon>mdi-page-layout-sidebar-right</v-icon> -->
       </v-list-item-action>
       <v-list-item-content>
         Flip layout
@@ -20,14 +21,11 @@ export default {
     },
     methods: {
         flipLayout: function () {
-        // console.log(getCookie("i18n_redirected"));
-        console.log(this.$getCookie("i18n_redirected"));
 
         // this functional changes which layout the component is using, as seen in the :root/layout folder
         let currentLayout = this.$nuxt.$data.layoutName;
         // either way works, though the 2nd one is harder to understand
         currentLayout == 'default' ? $nuxt.setLayout('flippedLayout') : $nuxt.setLayout('defaut');
-        // $nuxt.setLayout(currentLayout = currentLayout == 'default' ? 'flippedLayout' : 'default')
         }
     },
     components: {
