f9ec22f7c7f4dc0d71759e5c1ea9946ed6312286
Adam Simonini <adamsimonini@users.noreply.github.com>
2020-02-13 13:58:00 -0500

    quadruple conferences
    "That watermellon is for display purposes only!"

--- static/conferences.js
+++ static/conferences.js
@@ -97,4 +97,295 @@ export const conferences = [
         url: "https://drive.google.com/open?id=1wyLXvGYM9HXT429ZXFB9EqZSBppM625figQ2QZHgXA4",
       }
     },
+    {
+      title: "Archangel Summit 2020",
+      image: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F68822197%2F45360628177%2F1%2Foriginal.20190815-220126?w=800&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C120%2C1920%2C960&s=e6509fb0cca8e30a4ae749cc1a99fbaa",
+      // must be YYYY-MM-DD
+      startDate: "2020-10-18",
+      endDate: "2020-11-19",
+      location: {
+          city: "Ottawa",
+          country: "CA"
+      },
+      website: "archangelsummit.com/",
+      deadline: "31/08/2020",
+      reports: {
+        name: "Sarah",
+        url: "https://drive.google.com/open?id=1tRbm3gHuXDX_d1xI9TI3ZiPVwYBrRuZ2q7MHE-VVCJ8",
+      }
+    },
+    {
+      title: "An Evening with Eckhart Tolle in Toronto",
+      image: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F63007113%2F162678786657%2F1%2Foriginal.20190528-191634?w=800&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C13%2C1200%2C600&s=7785a7e83645db2fbe07a8ec4755a31a",
+      startDate: "2020-10-08",
+      endDate: "2020-10-10",
+      location: {
+        city: "Vancouver",
+        country: "CA"
+      },
+      website: "eventbrite.com/e/an-evening-with-eckhart-tolle-in-toronto-tickets-61455587316",
+      deadline: "2020-5-15",
+      reports: {
+        name: "Derek",
+        url: "https://drive.google.com/open?id=1j4kbjWy5mtRcHwYtjmwJl3axvXkxbtRtHtbTIl-4ZUs",
+      }
+    },
+    {
+      title: "Cannexus 20",
+      image: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F63579800%2F289770616254%2F1%2Foriginal.20190607-203204?w=1080&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C0%2C2160%2C1080&s=0e37f858a3124f04eed5f73e6a820767",
+      startDate: "2020-09-18",
+      endDate: "2020-09-18",
+      location: {
+          city: "Ottawa",
+          country: "CA"
+      },
+      website: "cannexus.ca/",
+      deadline: "01/31/2020",
+      reports: {
+        name: "Jennifer",
+        url: "https://drive.google.com/open?id=1E2QnyBMNjlyWE3cNAcfB4cpKXSTOVpSFiPtimXieUJk",
+      }
+    },
+    {
+      title: "TEDxKanata",
+      image: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F82962997%2F136856915032%2F1%2Foriginal.20191128-205332?w=1080&auto=format%2Ccompress&q=75&sharp=10&rect=226%2C0%2C2672%2C1336&s=e325637c32282b3b869a8e75305965c2",
+      startDate: "2021-09-18",
+      endDate: "2021-09-18",
+      location: {
+          city: "Palo Alto",
+          country: "US"
+      },
+      website: "tedxkanata.com/",
+      deadline: "2020-9-15",
+      reports: {
+        name: "Vlad",
+        url: "https://drive.google.com/open?id=1O15yvF4vvwnH87L-e0cH_Gq44DIKNgud73WbDA3csls",
+      }
+    },
+    {
+      title: "Nature-Based Climate Solutions Summit",
+      image: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F83785253%2F270664435685%2F1%2Foriginal.20191206-165559?w=1080&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C0%2C2160%2C1080&s=abe2aca336d1f100ed203e819b831b00",
+      startDate: "2021-11-18",
+      endDate: "2021-11-22",
+      location: {
+        city: "Calgary",
+        country: "CA"
+      },
+      website: "naturebasedclimatesolutions.ca",
+      deadline: "2020-10-01",
+      reports: {
+        name: "Rick",
+        url: "https://docs.google.com/document/d/1tRbm3gHuXDX_d1xI9TI3ZiPVwYBrRuZ2q7MHE-VVCJ8/edit?usp=sharing",
+      }
+    },
+    {
+      title: "Our Present Future",
+      image: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F77038979%2F184036823647%2F1%2Foriginal.20191016-143013?w=800&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C0%2C2000%2C1000&s=c687c75a58089d8d1b411cc52d961090",
+      startDate: "2020-012-18",
+      endDate: "2020-12-18",
+      location: {
+          city: "Palo Alto",
+          country: "US"
+      },
+      website: "",
+      deadline: "29/02/2020",
+      reports: {
+        name: "Morty",
+        url: "https://drive.google.com/open?id=1wyLXvGYM9HXT429ZXFB9EqZSBppM625figQ2QZHgXA4",
+      }
+    },
+    {
+      title: "Archangel Summit 2020",
+      image: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F68822197%2F45360628177%2F1%2Foriginal.20190815-220126?w=800&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C120%2C1920%2C960&s=e6509fb0cca8e30a4ae749cc1a99fbaa",
+      // must be YYYY-MM-DD
+      startDate: "2020-10-18",
+      endDate: "2020-11-19",
+      location: {
+          city: "Ottawa",
+          country: "CA"
+      },
+      website: "archangelsummit.com/",
+      deadline: "31/08/2020",
+      reports: {
+        name: "Sarah",
+        url: "https://drive.google.com/open?id=1tRbm3gHuXDX_d1xI9TI3ZiPVwYBrRuZ2q7MHE-VVCJ8",
+      }
+    },
+    {
+      title: "An Evening with Eckhart Tolle in Toronto",
+      image: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F63007113%2F162678786657%2F1%2Foriginal.20190528-191634?w=800&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C13%2C1200%2C600&s=7785a7e83645db2fbe07a8ec4755a31a",
+      startDate: "2020-10-08",
+      endDate: "2020-10-10",
+      location: {
+        city: "Vancouver",
+        country: "CA"
+      },
+      website: "eventbrite.com/e/an-evening-with-eckhart-tolle-in-toronto-tickets-61455587316",
+      deadline: "2020-5-15",
+      reports: {
+        name: "Derek",
+        url: "https://drive.google.com/open?id=1j4kbjWy5mtRcHwYtjmwJl3axvXkxbtRtHtbTIl-4ZUs",
+      }
+    },
+    {
+      title: "Cannexus 20",
+      image: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F63579800%2F289770616254%2F1%2Foriginal.20190607-203204?w=1080&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C0%2C2160%2C1080&s=0e37f858a3124f04eed5f73e6a820767",
+      startDate: "2020-09-18",
+      endDate: "2020-09-18",
+      location: {
+          city: "Ottawa",
+          country: "CA"
+      },
+      website: "cannexus.ca/",
+      deadline: "01/31/2020",
+      reports: {
+        name: "Jennifer",
+        url: "https://drive.google.com/open?id=1E2QnyBMNjlyWE3cNAcfB4cpKXSTOVpSFiPtimXieUJk",
+      }
+    },
+    {
+      title: "TEDxKanata",
+      image: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F82962997%2F136856915032%2F1%2Foriginal.20191128-205332?w=1080&auto=format%2Ccompress&q=75&sharp=10&rect=226%2C0%2C2672%2C1336&s=e325637c32282b3b869a8e75305965c2",
+      startDate: "2021-09-18",
+      endDate: "2021-09-18",
+      location: {
+          city: "Palo Alto",
+          country: "US"
+      },
+      website: "tedxkanata.com/",
+      deadline: "2020-9-15",
+      reports: {
+        name: "Vlad",
+        url: "https://drive.google.com/open?id=1O15yvF4vvwnH87L-e0cH_Gq44DIKNgud73WbDA3csls",
+      }
+    },
+    {
+      title: "Nature-Based Climate Solutions Summit",
+      image: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F83785253%2F270664435685%2F1%2Foriginal.20191206-165559?w=1080&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C0%2C2160%2C1080&s=abe2aca336d1f100ed203e819b831b00",
+      startDate: "2021-11-18",
+      endDate: "2021-11-22",
+      location: {
+        city: "Calgary",
+        country: "CA"
+      },
+      website: "naturebasedclimatesolutions.ca",
+      deadline: "2020-10-01",
+      reports: {
+        name: "Rick",
+        url: "https://docs.google.com/document/d/1tRbm3gHuXDX_d1xI9TI3ZiPVwYBrRuZ2q7MHE-VVCJ8/edit?usp=sharing",
+      }
+    },
+    {
+      title: "Our Present Future",
+      image: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F77038979%2F184036823647%2F1%2Foriginal.20191016-143013?w=800&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C0%2C2000%2C1000&s=c687c75a58089d8d1b411cc52d961090",
+      startDate: "2020-012-18",
+      endDate: "2020-12-18",
+      location: {
+          city: "Palo Alto",
+          country: "US"
+      },
+      website: "",
+      deadline: "29/02/2020",
+      reports: {
+        name: "Morty",
+        url: "https://drive.google.com/open?id=1wyLXvGYM9HXT429ZXFB9EqZSBppM625figQ2QZHgXA4",
+      }
+    },
+    {
+      title: "Archangel Summit 2020",
+      image: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F68822197%2F45360628177%2F1%2Foriginal.20190815-220126?w=800&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C120%2C1920%2C960&s=e6509fb0cca8e30a4ae749cc1a99fbaa",
+      // must be YYYY-MM-DD
+      startDate: "2020-10-18",
+      endDate: "2020-11-19",
+      location: {
+          city: "Ottawa",
+          country: "CA"
+      },
+      website: "archangelsummit.com/",
+      deadline: "31/08/2020",
+      reports: {
+        name: "Sarah",
+        url: "https://drive.google.com/open?id=1tRbm3gHuXDX_d1xI9TI3ZiPVwYBrRuZ2q7MHE-VVCJ8",
+      }
+    },
+    {
+      title: "An Evening with Eckhart Tolle in Toronto",
+      image: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F63007113%2F162678786657%2F1%2Foriginal.20190528-191634?w=800&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C13%2C1200%2C600&s=7785a7e83645db2fbe07a8ec4755a31a",
+      startDate: "2020-10-08",
+      endDate: "2020-10-10",
+      location: {
+        city: "Vancouver",
+        country: "CA"
+      },
+      website: "eventbrite.com/e/an-evening-with-eckhart-tolle-in-toronto-tickets-61455587316",
+      deadline: "2020-5-15",
+      reports: {
+        name: "Derek",
+        url: "https://drive.google.com/open?id=1j4kbjWy5mtRcHwYtjmwJl3axvXkxbtRtHtbTIl-4ZUs",
+      }
+    },
+    {
+      title: "Cannexus 20",
+      image: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F63579800%2F289770616254%2F1%2Foriginal.20190607-203204?w=1080&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C0%2C2160%2C1080&s=0e37f858a3124f04eed5f73e6a820767",
+      startDate: "2020-09-18",
+      endDate: "2020-09-18",
+      location: {
+          city: "Ottawa",
+          country: "CA"
+      },
+      website: "cannexus.ca/",
+      deadline: "01/31/2020",
+      reports: {
+        name: "Jennifer",
+        url: "https://drive.google.com/open?id=1E2QnyBMNjlyWE3cNAcfB4cpKXSTOVpSFiPtimXieUJk",
+      }
+    },
+    {
+      title: "TEDxKanata",
+      image: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F82962997%2F136856915032%2F1%2Foriginal.20191128-205332?w=1080&auto=format%2Ccompress&q=75&sharp=10&rect=226%2C0%2C2672%2C1336&s=e325637c32282b3b869a8e75305965c2",
+      startDate: "2021-09-18",
+      endDate: "2021-09-18",
+      location: {
+          city: "Palo Alto",
+          country: "US"
+      },
+      website: "tedxkanata.com/",
+      deadline: "2020-9-15",
+      reports: {
+        name: "Vlad",
+        url: "https://drive.google.com/open?id=1O15yvF4vvwnH87L-e0cH_Gq44DIKNgud73WbDA3csls",
+      }
+    },
+    {
+      title: "Nature-Based Climate Solutions Summit",
+      image: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F83785253%2F270664435685%2F1%2Foriginal.20191206-165559?w=1080&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C0%2C2160%2C1080&s=abe2aca336d1f100ed203e819b831b00",
+      startDate: "2021-11-18",
+      endDate: "2021-11-22",
+      location: {
+        city: "Calgary",
+        country: "CA"
+      },
+      website: "naturebasedclimatesolutions.ca",
+      deadline: "2020-10-01",
+      reports: {
+        name: "Rick",
+        url: "https://docs.google.com/document/d/1tRbm3gHuXDX_d1xI9TI3ZiPVwYBrRuZ2q7MHE-VVCJ8/edit?usp=sharing",
+      }
+    },
+    {
+      title: "Our Present Future",
+      image: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F77038979%2F184036823647%2F1%2Foriginal.20191016-143013?w=800&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C0%2C2000%2C1000&s=c687c75a58089d8d1b411cc52d961090",
+      startDate: "2020-012-18",
+      endDate: "2020-12-18",
+      location: {
+          city: "Palo Alto",
+          country: "US"
+      },
+      website: "",
+      deadline: "29/02/2020",
+      reports: {
+        name: "Morty",
+        url: "https://drive.google.com/open?id=1wyLXvGYM9HXT429ZXFB9EqZSBppM625figQ2QZHgXA4",
+      }
+    },
 ];
