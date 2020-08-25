1d04022f6313f5af9fa5cf70cdd1a289cba4aebc
Adam Simonini <adamsimonini@users.noreply.github.com>
2020-08-24 22:50:31 -0400

    adding filtering capabilities

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <!-- <div v-if="!loaded">
       <v-progress-circular :size="100" color="primary" indeterminate class="loader"></v-progress-circular>
     </div>-->
-    <div class="loader-box" v-if="!loaded" >
+    <div class="loader-box" v-if="!loaded">
       <v-progress-circular :size="100" color="primary" indeterminate class="loader"></v-progress-circular>
     </div>
     <v-flex xs12 sm8 md6 v-if="loaded">
@@ -11,6 +11,36 @@
       <nuxt-link :to="localePath('inspire')">inspire</nuxt-link>-->
 
       <h2 class="text-center">{{ $t('allConferencesTitle')}}</h2>
+      <div>
+        <v-row>
+          <v-spacer></v-spacer>
+          <v-col cols="2">
+            <v-btn @click="filterDate()">
+              See {{sortDateOrder == 'oldest' ? "newest" : "oldest"}} first
+              <v-icon v-if="sortDateOrder == 'oldest'">mdi-arrow-up-thick</v-icon>
+              <v-icon v-if="sortDateOrder == 'newest'">mdi-arrow-down-thick</v-icon>
+            </v-btn>
+          </v-col>
+          <!-- <v-col cols="2">
+            <v-select
+              @click="getCities()"
+              v-model="selectedCity"
+              :items="cities"
+              menu-props="auto"
+              label="Filter By City"
+              hide-details
+              prepend-icon="map-marker"
+              single-line
+            ></v-select>
+          </v-col>-->
+          <v-col cols="2">
+            <v-btn color="warning" @click="reset()">
+              Reset
+              <v-icon>mdi-circle</v-icon>
+            </v-btn>
+          </v-col>
+        </v-row>
+      </div>
       <div class="all-conferneces">
         <ConfCard
           v-for="(conf) in conferences"
@@ -34,6 +64,7 @@ import ConfCard from '@c/confCard.vue'
 import { mapState } from 'vuex'
 // import { conferences } from '@@/static/conferences.js'
 import firebase from 'firebase'
+import moment from 'moment'
 
 if ('indexedDB' in window) {
   console.log('This browser does support IndexedDB')
@@ -45,7 +76,11 @@ export default {
     return {
       conferences: [],
       imageUrl: [],
-      loaded: false
+      loaded: false,
+      sortDateOrder: 'newest',
+      cities: [],
+      allConferences: [],
+      selectedCity: null
     }
   },
   layout: 'default',
@@ -71,6 +106,7 @@ export default {
           imageRef: conference.data().imageRef
         })
       }
+      this.allConferences = this.conferences.slice()
       setTimeout(() => {
         this.loaded = true
       }, 1000)
@@ -78,7 +114,53 @@ export default {
     conferences()
   },
   mounted() {},
-  methods: {}
+  methods: {
+    filterDate() {
+      console.log(this.conferences)
+      let sortedArray = []
+      switch (this.sortDateOrder) {
+        case 'newest':
+          console.log('newest triggered')
+          sortedArray = this.conferences.sort(
+            (a, b) => new Date(a.startDate) - new Date(b.startDate)
+          )
+          this.sortDateOrder = 'oldest'
+          break
+
+        case 'oldest':
+          console.log('oldest triggered')
+          sortedArray = this.conferences.sort(
+            (a, b) => new Date(b.startDate) - new Date(a.startDate)
+          )
+          this.sortDateOrder = 'newest'
+          break
+        default:
+          console.log('default triggered')
+          break
+      }
+      this.conferences = sortedArray
+      // this.conferences = []
+    },
+    ottawaOnly() {
+      this.conferences = this.conferences.filter(conf => conf.city == 'Ottawa')
+    },
+    getCities() {
+      if (this.cities.length == 0) {
+        this.cities = this.conferences.map(conf => {
+          return conf.city
+        })
+        console.log(this.cities)
+      } else {
+        let filteredByCity = this.conferences.filter(conf => {
+          return conf.city == this.selectedCity
+        })
+        this.conferences = filteredByCity
+      }
+    },
+    reset() {
+      this.conferences = this.allConferences.slice()
+    }
+  }
 }
 </script>
 
